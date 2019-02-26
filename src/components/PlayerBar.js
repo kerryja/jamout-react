a5e9267c340b125cbae5cb1929d83e34b5148773
Kerry <kerry.jackson108@gmail.com>
2019-02-25 19:10:24 -0500

    checkpoint 2760 player bar buttons: complete

---
+++ src/components/PlayerBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class PlayerBar extends Component {
+  render() {
+    return (
+      <section className="player-bar">
+        <section id="buttons">
+          <button id="previous" onClick={this.props.handlePrevClick}>
+            <span className="icon ion-md-skip-backward" />
+          </button>
+          <button id="play-pause" onClick={this.props.handleSongClick}>
+            <span
+              className={
+                this.props.isPlaying ? "icon ion-md-pause" : "icon ion-md-play"
+              }
+            />
+          </button>
+          <button id="next">
+            <span className="icon ion-md-skip-forward" />
+          </button>
+        </section>
+        <section id="time-control">
+          <div className="current-time">-:--</div>
+          <input type="range" className="seek-bar" value="80" />
+          <div className="icon ion-volume-high" />
+        </section>
+      </section>
+    );
+  }
+}
+
+export default PlayerBar;
